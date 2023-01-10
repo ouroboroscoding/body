885884566d672d233f821915ba246c4841aea9ec
Chris Nasr <chris@ouroboroscoding.com>
2023-01-10 10:39:46 -0500

    - Initial commit

---
+++ body/__init__.py
@@ -0,0 +1,11 @@
+# coding=utf8
+"""Body
+
+Shared methods for accessing the brain and other shared formats
+"""
+
+__author__		= "Chris Nasr"
+__copyright__	= "Ouroboros Coding Inc"
+__version__		= "1.0.0"
+__email__		= "chris@ouroboroscoding.com"
+__created__		= "2022-08-29"
