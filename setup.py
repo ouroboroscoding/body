27712a55a325a8565f09d3e539b8fbf08ce9d2ad
Chris Nasr <chris@ouroboroscoding.com>
2023-01-10 15:27:56 -0500

    - Fixed company name in LICENSE - Added setup.cfg file - Renamed module from Body to Body-OC - Fixed requirement of Rest-OC

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as oF:
 	long_description=oF.read()
 
 setup(
-	name='Body',
+	name='Body-OC',
 	version='0.0.1',
 	description='Body contains shared concepts among all body parts',
 	long_description=long_description,
@@ -21,6 +21,6 @@ setup(
 	license='Custom',
 	packages=['body'],
 	python_requires='>=3.10',
-	install_requires=['restoc==1.0.0'],
+	install_requires=['Rest-OC==1.0.0'],
 	zip_safe=True
 )
