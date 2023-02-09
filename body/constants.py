1d759b0ba39a997a977ddbc862b8b7251bdd5d71
Chris Nasr <chris@ouroboroscoding.com>
2023-02-09 14:01:18 -0500

    - Added constants module for common constants

---
+++ body/constants.py
@@ -0,0 +1,15 @@
+# coding=utf8
+""" Constants
+
+Shared constants
+"""
+
+__author__ = "Chris Nasr"
+__copyright__ = "Ouroboros Coding Inc"
+__version__ = "1.0.0"
+__email__ = "chris@ouroboroscoding.com"
+__created__ = "2023-03-09"
+
+SECONDS_HOUR = 3600
+SECONDS_DAY = 86400
+SECONDS_WEEK = 604800
